41de43143f90c4b439ba287c24df6fa23afa7a70
torinmo adebayo <torinmo.work@gmail.com>
2024-02-02 00:30:21 +0100

    responsive tweaks

--- src/component/Blogsection.jsx
+++ src/component/Blogsection.jsx
@@ -8,8 +8,8 @@ const Blogsection = (props) => {
     <div className="w-full h-full bg-[#C4942D] mb-[200px]  ">
         <div className='md:w-[1028px] max-w-[600px] md:h-[600px] h-[700px]  md:mt-[150px] md:pt-[50px] pt-[30px] '>
             <div className=' md:pl-[0px]   '>
-                <p className='text-white text-[11px] pb-[10px] w-[200px]  md:ml-[580px] ml-[180px] '>{props.title}</p>
-                <h1 className='md:text-3xl text-lg md:ml-[270px] ml-[30px] md:w-[700px] w-[350px] pb-[60px] text-white text-center '>{props.body}</h1>
+                <p className='text-white text-[11px] pb-[10px] md:w-[200px]  md:ml-[580px] ml-[180px] '>{props.title}</p>
+                <h1 className='md:text-3xl text-lg md:ml-[270px] ml-[50px] md:w-[700px] w-[300px] pb-[60px] text-white text-center '>{props.body}</h1>
                 
             </div>
             <div className=' flex md:flex-row flex-col md:ml-[%] '>
--- src/component/Headline2.jsx
+++ src/component/Headline2.jsx
@@ -11,11 +11,11 @@ function Headline2 (props) {
                     <img src={props.img} alt="" className='w-12 h-6 mt-[40px]' />
                     <img src={props.png} className='pl-[70px] md:h-[100px] h-[60px]' alt="" />
                  </div>
-                 <h1 className='md:text-3xl text-base md:w-[400px] w-[350px] md:mt-[-20px] md:text-left text-center md:pb-[20px] pb-[20px] leading-10' >Design and construction of head over bridge </h1>
-                 <p className='md:text-base text-xs md:w-[500px] w-[350px]'>International bewrey plc,Lagos state</p>
+                 <h1 className='md:text-3xl text-base md:w-[400px] w-[330px] md:mt-[-20px] md:text-left text-center md:pb-[20px] pb-[20px] leading-10' >Design and construction of head over bridge </h1>
+                 <p className='md:text-base text-xs md:w-[500px] w-[330px]'>International bewrey plc,Lagos state</p>
             </div>
            <div>
-                <p className='md:text-base text-xs md:w-[500px] w-[350px] pt-[30px]'>Nexant Consulting Limited is an Indigenous ISO Certified Consultancy Firm in Engineering Design & Construction Supervisions. Nexant is also the 1st and Currently still the only Indigenous ISO Certified Railway Consultancy Firm in Nigeria, we are a registered member of the Council for the Regulation of Engineering in Nigeria (COREN) and Association for Consulting Engineering in Nigeria (ACEN).</p>
+                <p className='md:text-base text-xs md:w-[500px] w-[330px] pt-[30px]'>Nexant Consulting Limited is an Indigenous ISO Certified Consultancy Firm in Engineering Design & Construction Supervisions. Nexant is also the 1st and Currently still the only Indigenous ISO Certified Railway Consultancy Firm in Nigeria, we are a registered member of the Council for the Regulation of Engineering in Nigeria (COREN) and Association for Consulting Engineering in Nigeria (ACEN).</p>
            </div>
 
         </div>
