95ac935bbb1322ebe8629244cef241137ee10b67
torinmo <torinmo.work@gmailcom>
2025-06-02 15:47:16 +0100

    instructions in the index.js and app.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
             
         </Routes>
       </Router>
-      {/* how far ayobami when u have this code you should see this the best way to structure your code is to separate some components and pages
+      {/* how far when u have this code you should see this the best way to structure your code is to separate some components and pages
           the components folder will hold any component folder some components such as footer and navbar and some cards that are repititive 
           pages folder will hold all the routes that are in the folder i will help you with all the routes all you need is just to solve the ui
         
@@ -38,7 +38,10 @@ function App() {
       {/* 
           for all the pages or components that you want to create make use of the .jsx extension for it to go well it has some benefits over using .js 
 
-          always store the all the pictures in the public folder dont create another folder for pictures
+          always store the all the pictures in the assets folder and import them in the component or page that you want to use them
+          always use the Link component from react-router-dom to navigate between pages and components
+          
+          all pages routes are in the pages folder and all components are in the component folder
 
       */}
     </div>
