86f144cdac9127a1e8fda13b3a81865239b9c490
Badmus Emmanuel <badmusemmanuel77@gmail.com>
2025-07-10 12:25:45 +0100

    emmeax

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 import Service from './pages/Service';
 import Project from './pages/Project';
 import QA from './pages/QA';
-import JobPage from './pages/Career';
+import Career from './pages/Career';
 import JobDetailsPage from './pages/JobDetailsPage';
 import JobApplicationForm from './pages/JobApplicationForm';
 // import Maintenance from './pages/Maintenance'
@@ -25,7 +25,7 @@ function App() {
             <Route path='/blog' element={<Blog />} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/qa' element={<QA />} />
-            <Route path='/jobpage' element={<JobPage />} />
+            <Route path='/jobpage' element={<Career />} />
             <Route path="/careers/:jobId" element={<JobDetailsPage />} />
             <Route path="/careers/:jobId/apply" element={<JobApplicationForm />} />
         </Routes>
